0350de15a7aa8d75ccd6a3aed9cc33c3da61a4cb
Aayush Prinja <aayushprinja763@gmail.com>
2025-11-03 19:36:35 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,11 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userid/:name/:password", (req, res) => {
-  console.log(req.params);
-
-  res.send({ firstname: "Akshay", lastName: "Prinja" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("handling the route user 1");
+    next();
+  },
+  (req, res, next) => {
+    //res.send("2nd response");
+    console.log("handling the route user 2");
+    next();
+  },
+  (req, res, next) => {
+    //res.send("3rd response");
+    console.log("handling the route user 3");
+    next();
+  },
+  (req, res, next) => {
+    //res.send("4th response");
+    console.log("handling the route user 4");
+  }
+);
 
 app.listen(7777, () => {
   console.log("express sever created 7777...");
