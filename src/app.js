70f35edcb9b2b266bd3537b22ea0ff07c4f2d870
Aayush Prinja <aayushprinja763@gmail.com>
2025-11-03 17:32:10 +0530

    request handlers

--- src/app.js
+++ src/app.js
@@ -2,16 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Namaste Aayush");
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Akshay", lastName: "Prinja" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data saved successfully to DB");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("deleted successfully");
 });
 
 app.use("/test", (req, res) => {
   res.send("Hello from express server");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("hello,hello,hello");
 });
 
 app.listen(7777, () => {
