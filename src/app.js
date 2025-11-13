a4d321bb24183b6c257e4817b651576f247f5c44
Aayush Prinja <aayushprinja763@gmail.com>
2025-11-13 14:01:03 +0530

    login api with sanitisation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const { adminAuth, userAuth } = require("./middlewares/auth");
+const bcrypt = require("bcryptjs");
 const User = require("./models/user");
 
 const app = express();
@@ -9,13 +10,45 @@ app.use(express.json());
 
 //api to insert data in user schema
 app.post("/signup", async (req, res) => {
+  const { firstName, lastName, emailId, password } = req.body;
+  //validation of the data
+  // validations done on schema level
+  // encrypt the password
+  const passwordHash = await bcrypt.hash(password, 10);
+  console.log(passwordHash);
+
   //creating a user and saving it in database
-  const user = new User(req.body);
+  const user = new User({
+    firstName,
+    lastName,
+    emailId,
+    password: passwordHash,
+  });
   try {
     await user.save();
     res.send("User created successfully");
   } catch (error) {
     res.status(400).send("Error saving the user:" + error.message);
+  }
+});
+
+// login api
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+
+    if (!user) {
+      res.send("invalid credential");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.send("User Login Successful");
+    } else {
+      res.status(400).send("invalid credential");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema(
     },
     lastName: {
       type: String,
+      required: true,
     },
     emailId: {
       type: String,
